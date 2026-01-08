43f02bebe880bc4e159d7bc5c0397003de6f9e44
bahar <88434002+bahar-dc@users.noreply.github.com>
2026-01-08 15:37:51 +0500

    Guide Links update for hitch measurements

--- wp-content/themes/stone-stomper/templates/template-stone-stomper.php
+++ wp-content/themes/stone-stomper/templates/template-stone-stomper.php
@@ -458,7 +458,7 @@ list( $sts_var_post_id, $sts_fields, $sts_option_fields ) = StoneStomper::defaul
 											</div>
 											<fieldset class="hitch_measurement_dropdown" style="display:none">
 												<div class="note notice-bar mt-0">
-													Additional Measurement required for this bar option. Please refer to the image gallery to determine how to find this length. Alternatively, view instructions for "Option 2" ( <a href="https://stonestomper.com.au/wp-admin/upload.php?item=2177">https://stonestomper.com.au/wp-admin/upload.php?item=2177</a> )  or Option 3" ( <a href="https://stonestomper.com.au/wp-admin/upload.php?item=2176">https://stonestomper.com.au/wp-admin/upload.php?item=2176</a> ).
+													Additional Measurement required for this bar option. Please refer to the image gallery to determine how to find this length. Alternatively, view instructions for "Option 2" ( <a href="https://stonestomper.com.au/wp-content/uploads/2025/12/Option-2-Measurement.png" target="_blank">Link</a> )  or Option 3" ( <a href="https://stonestomper.com.au/wp-content/uploads/2025/12/Option-3-Measurement.jpg" target="_blank">Link</a> ).
 												</div>
 												<div class="field">
 													<label class="req screen-reader-text" for="additional_hitch_measurement">Additional Measurement required for this bar option.</label>
