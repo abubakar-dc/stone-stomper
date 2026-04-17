7990e4eaa4e9de9d268bea1990a07c08920335f5
bahar <88434002+bahar-dc@users.noreply.github.com>
2026-04-17 20:32:19 +0500

    Bar option question value fetch into the csutomer cpt

--- wp-content/themes/stone-stomper/templates/template-stone-stomper.php
+++ wp-content/themes/stone-stomper/templates/template-stone-stomper.php
@@ -498,6 +498,8 @@ list( $sts_var_post_id, $sts_fields, $sts_option_fields ) = StoneStomper::defaul
 												<option value="no" data-bar-option-value="<?php echo esc_attr($sts_var_bar_question_4_bar_option_value_no); ?>">No</option>
 											</select>
 										</div>
+										<!-- Hidden field to store bar option value from JavaScript -->
+										<input type="hidden" name="question_bar_option_value" id="question_bar_option_value" value="">
 										<fieldset class="hitch_measurement_dropdown" style="display:none">
 											<?php if($sts_var_section_bar_options_description_option_two){ ?>
 												<div id="option-two-description" class="note notice-bar option-two-description mt-0"  style="display:none">
